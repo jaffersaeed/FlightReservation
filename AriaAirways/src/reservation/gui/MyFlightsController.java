7945c6f13f11e5cada7cf4e4ab3c202b9234ef44
nicolettepowell <nicolettepowell@localhost>
2022-12-28 15:08:59 -0500

    up

--- AriaAirways/src/reservation/gui/MyFlightsController.java
+++ AriaAirways/src/reservation/gui/MyFlightsController.java
@@ -1,5 +1,39 @@
 package application;
 
-public class MyFlightsController {
-
+public class MyFlightsController implements Initializable {
+	@FXML
+	private TableView<Flight, Integer> table;
+	@FXML
+	private TableColumn<Flight, Integer> flightIdColumn;
+	@FXML
+	private TableColumn<Flight, Integer> originCityColumn;
+	@FXML
+	private TableColumn<Flight, Integer> departingCity;
+	@FXML
+	private TableColumn<Flight, Integer> departingColumn; // depart time
+	@FXML
+	private TableColumn<Flight, Integer> capacityColumn;
+	@FXML
+	private TableColumn<Flight, Integer> remainingColumn;
+	@FXML
+	private Button back;
+	
+	public void goBack(ActionEvent event) throws IOException {
+    	 Main m = new Main();
+	        m.changeScene("MainMenu.fxml");
+    }
+	
+	@Override
+	public void initalize(URL url, ResourceBundle rn) {
+		
+		flightIdColumn.setCellValueFactory(new PropertyValueFactory<>("flightNumber"));
+		originCityColumn.setCellValueFactory(new PropertyValueFactory<>("departureCity"));
+		departingCity.setCellValueFactory(new PropertyValueFactory<>("destinationCity"));
+		departureColumn.setCellValueFactory(new PropertyValueFactory<>("departureDate"));
+		capcityColumn.setCellValueFactory(new PropertyValueFactory<>("capacity"));
+		remainingColumn.setCellValueFactory(new PropertyValueFactory<>("passengerCount"));
 }
+		public ObservableList<Flight> getBooking(){
+		ObservableList<Flight> flight= SQL.getBooking();
+		return flight;
+	}
